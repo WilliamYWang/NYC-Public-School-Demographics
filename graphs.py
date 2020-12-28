8d00eaecae8ac182b1d31cdf38cfe432f735a198
WilliamYWang <williamwang0223@gmail.com>
2020-12-27 23:49:17 -0500

    Changes variable names, added webapp.py

--- graphs.py
+++ graphs.py
@@ -9,18 +9,18 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('dataset1.csv')
+dataset1 = pd.read_csv('dataset1.csv')
 
 @app.callback(
     Output('graph1', 'figure'),
     Input('dropdown1', 'value'))
 def update_figure1(selected_variable):
     if selected_variable == 'year':
-        s = df['year'].value_counts()  # returns series containing counts of unique values
-        dfg = pd.DataFrame({'Year': s.index, 'Frequency': s.values})  # converts series to pandas dataframe with two columns (year, frequency)
-        figure = px.bar(dfg, x='Year', y='Frequency', title='Bar chart of ' + selected_variable)
+        s = dataset1['year'].value_counts()  # returns series containing counts of unique values
+        tft = pd.DataFrame({'Year': s.index, 'Frequency': s.values})  # converts series to pandas dataframe with two columns (year, frequency)
+        figure = px.bar(tft, x='Year', y='Frequency', title='Bar chart of ' + selected_variable)
     else:
-        figure = px.histogram(df, x=selected_variable, nbins=15, title='Histogram of ' + selected_variable)  # selected_variable is the value
+        figure = px.histogram(dataset1, x=selected_variable, nbins=15, title='Histogram of ' + selected_variable)  # selected_variable is the value
     return figure
 
 @app.callback(
@@ -28,55 +28,55 @@ def update_figure1(selected_variable):
     Input('dropdown2', 'value'))
 def update_figure2(selected_variable):
     if selected_variable == 'year':
-        df['year_group'] = pd.cut(df['year'], bins=[2005, 2007, 2009, 2011, 2014, 2016, 2018],
+        dataset1['year_group'] = pd.cut(dataset1['year'], bins=[2005, 2007, 2009, 2011, 2014, 2016, 2018],
         labels=['2006-2007', '2008-2009', '2010-2011', '2012-2014', '2015-2016', '2017-2018']) # bins values into intervals
-        s = df['year_group'].value_counts()
-        dfg = pd.DataFrame({'Years': s.index, 'Count': s.values})
-        figure = px.pie(dfg, names='Years', values='Count', title="Years")
+        s = dataset1['year_group'].value_counts()
+        tft = pd.DataFrame({'Years': s.index, 'Count': s.values})
+        figure = px.pie(tft, names='Years', values='Count', title="Years")
     elif selected_variable == 'total_enrollment':
-        df['enrollment_group'] = pd.cut(df['total_enrollment'], bins=[0, 499, 999, 1499, 1999, 4999],
+        dataset1['enrollment_group'] = pd.cut(dataset1['total_enrollment'], bins=[0, 499, 999, 1499, 1999, 4999],
         labels=['0-499', '500-999', '1000-1499', '1500-1999', '2000+'], include_lowest=True)
-        s = df['enrollment_group'].value_counts()
-        dfg = pd.DataFrame({'Enrollment Number': s.index, 'Frequency': s.values})
-        figure = px.pie(dfg, names='Enrollment Number', values='Frequency', title="Enrollment Count")
+        s = dataset1['enrollment_group'].value_counts()
+        tft = pd.DataFrame({'Enrollment Number': s.index, 'Frequency': s.values})
+        figure = px.pie(tft, names='Enrollment Number', values='Frequency', title="Enrollment Count")
     elif selected_variable == 'female_num':
-        df['female_group'] = pd.cut(df['female_num'], bins=[0, 199, 399, 599, 799, 999, 2399],
+        dataset1['female_group'] = pd.cut(dataset1['female_num'], bins=[0, 199, 399, 599, 799, 999, 2399],
         labels=['0-199', '200-399', '400-599', '600-799', '800-999', '1000+'], include_lowest=True)
-        s = df['female_group'].value_counts()
-        dfg = pd.DataFrame({'Female Students': s.index, 'Frequency': s.values})
-        figure = px.pie(dfg, names='Female Students', values='Frequency', title="Frequency of Female Students")
+        s = dataset1['female_group'].value_counts()
+        tft = pd.DataFrame({'Female Students': s.index, 'Frequency': s.values})
+        figure = px.pie(tft, names='Female Students', values='Frequency', title="Frequency of Female Students")
     elif selected_variable == 'male_num':
-        df['male_group'] = pd.cut(df['male_num'], bins=[0, 199, 399, 599, 799, 999, 2599],
+        dataset1['male_group'] = pd.cut(dataset1['male_num'], bins=[0, 199, 399, 599, 799, 999, 2599],
         labels=['0-199', '200-399', '400-599', '600-799', '800-999', '1000+'], include_lowest=True)
-        s = df['male_group'].value_counts()
-        dfg = pd.DataFrame({'Male Students': s.index, 'Frequency': s.values})
-        figure = px.pie(dfg, names='Male Students', values='Frequency', title="Frequency of Male Students")
+        s = dataset1['male_group'].value_counts()
+        tft = pd.DataFrame({'Male Students': s.index, 'Frequency': s.values})
+        figure = px.pie(tft, names='Male Students', values='Frequency', title="Frequency of Male Students")
     elif selected_variable == 'asian_num':
-        df['asian_group'] = pd.cut(df['asian_num'], bins=[0, 199, 399, 599, 799, 1999],
+        dataset1['asian_group'] = pd.cut(dataset1['asian_num'], bins=[0, 199, 399, 599, 799, 1999],
         labels=['0-199', '200-399', '400-599', '600-799', '800+'], include_lowest=True)
-        s = df['asian_group'].value_counts()
-        dfg = pd.DataFrame({'Asian Students': s.index, 'Frequency': s.values})
-        figure = px.pie(dfg, names='Asian Students', values='Frequency', title="Frequency of Asian Students")
+        s = dataset1['asian_group'].value_counts()
+        tft = pd.DataFrame({'Asian Students': s.index, 'Frequency': s.values})
+        figure = px.pie(tft, names='Asian Students', values='Frequency', title="Frequency of Asian Students")
     elif selected_variable == 'black_num':
-        df['black_group'] = pd.cut(df['black_num'], bins=[0, 199, 399, 599, 799, 2599],
+        dataset1['black_group'] = pd.cut(dataset1['black_num'], bins=[0, 199, 399, 599, 799, 2599],
         labels=['0-199', '200-399', '400-599', '600-799', '800+'], include_lowest=True)
-        s = df['black_group'].value_counts()
-        dfg = pd.DataFrame({'Black Students': s.index, 'Frequency': s.values})
-        figure = px.pie(dfg, names='Black Students', values='Frequency', title="Frequency of Black Students")
+        s = dataset1['black_group'].value_counts()
+        tft = pd.DataFrame({'Black Students': s.index, 'Frequency': s.values})
+        figure = px.pie(tft, names='Black Students', values='Frequency', title="Frequency of Black Students")
     elif selected_variable == 'hispanic_num':
-        df['hispanic_group'] = pd.cut(df['hispanic_num'], bins=[0, 199, 399, 599, 799, 2599],
+        dataset1['hispanic_group'] = pd.cut(dataset1['hispanic_num'], bins=[0, 199, 399, 599, 799, 2599],
         labels=['0-199', '200-399', '400-599', '600-799', '800+'], include_lowest=True)
-        s = df['hispanic_group'].value_counts()
-        dfg = pd.DataFrame({'Hispanic Students': s.index, 'Frequency': s.values})
-        figure = px.pie(dfg, names='Hispanic Students', values='Frequency', title="Frequency of Hispanic Students")
+        s = dataset1['hispanic_group'].value_counts()
+        tft = pd.DataFrame({'Hispanic Students': s.index, 'Frequency': s.values})
+        figure = px.pie(tft, names='Hispanic Students', values='Frequency', title="Frequency of Hispanic Students")
     elif selected_variable == 'white_num':
-        df['white_group'] = pd.cut(df['white_num'], bins=[0, 199, 399, 599, 799, 3499],
+        dataset1['white_group'] = pd.cut(dataset1['white_num'], bins=[0, 199, 399, 599, 799, 3499],
         labels=['0-199', '200-399', '400-599', '600-799', '800+'], include_lowest=True)
-        s = df['white_group'].value_counts()
-        dfg = pd.DataFrame({'White Students': s.index, 'Frequency': s.values})
-        figure = px.pie(dfg, names='White Students', values='Frequency', title="Frequency of White Students")
+        s = dataset1['white_group'].value_counts()
+        tft = pd.DataFrame({'White Students': s.index, 'Frequency': s.values})
+        figure = px.pie(tft, names='White Students', values='Frequency', title="Frequency of White Students")
     else:
-        figure = px.pie(df, names=selected_variable)
+        figure = px.pie(dataset1, names=selected_variable)
 
     return figure
 
@@ -84,7 +84,7 @@ def update_figure2(selected_variable):
     Output('graph3', 'figure'),
     [Input('x-axis', 'value'), Input('y-axis', 'value')])
 def update_figure3(xaxis, yaxis):
-    figure = px.scatter(df, x=df[xaxis], y=df[yaxis], hover_name='name', title='Scatter plot of ' + xaxis + ' vs ' + yaxis)
+    figure = px.scatter(dataset1, x=dataset1[xaxis], y=dataset1[yaxis], hover_name='name', title='Scatter plot of ' + xaxis + ' vs ' + yaxis)
     figure.update_xaxes(title=xaxis)
     figure.update_yaxes(title=yaxis)
     return figure
--- webapp.py
+++ webapp.py
@@ -13,13 +13,13 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-dataset2 = pd.read_csv('dataset2.csv')
-dataset3 = pd.read_csv('dataset3.csv')
+df = pd.read_csv('dataset2.csv')
+df1 = pd.read_csv('dataset3.csv')
 
 # PCA Analysis
 n_components = 2
 pca = PCA(n_components=n_components)
-components = pca.fit_transform(dataset2)
+components = pca.fit_transform(df)
 total_var = pca.explained_variance_ratio_.sum() * 100
 labels = {
     str(i): f"PC {i+1} ({var:.1f}%)"
@@ -28,7 +28,7 @@ labels = {
 
 # Scree Plot
 pca1 = PCA()
-pc = pca1.fit_transform(dataset2)
+pc = pca1.fit_transform(df)
 sp = pd.DataFrame({'Variance Explained (%)': pca1.explained_variance_ratio_*100, 'Principle Component': ['PC1','PC2','PC3','PC4','PC5','PC6','PC7']})
 
 # Biplot
@@ -52,11 +52,11 @@ for i, feature in enumerate(features):
     )
 
 # MDS
-similarities = pairwise_distances(dataset2, metric='euclidean')
-mdsg = MDS(n_components=2, metric=True, dissimilarity='precomputed')
-pos = mdsg.fit_transform(similarities)
-dissimilarities = pairwise_distances(dataset2.corr(method='pearson'), metric='correlation')
-sop = mdsg.fit_transform(dissimilarities)
+similarities = pairwise_distances(df, metric='euclidean')
+mdsdf = MDS(n_components=2, metric=True, dissimilarity='precomputed')
+pos = mdsdf.fit_transform(similarities)
+dissimilarities = pairwise_distances(df.corr(method='pearson'), metric='correlation')
+sop = mdsdf.fit_transform(dissimilarities)
 
 @app.callback(Output('page-content', 'children'),
               Input('url', 'pathname'))
@@ -75,6 +75,12 @@ def display_page(pathname):
         return mds_data_layout
     elif pathname == '/MDS-display-attributes':
         return mds_attributes_layout
+    elif pathname == '/basic-visualizations':
+        return basic_visualizations_layout
+    elif pathname == '/advanced-displays':
+        return advanced_displays_layout
+    elif pathname == '/interactive-dashboard':
+        return interactive_dashboard_layout
     else:
         return index_page
 
@@ -84,26 +90,18 @@ app.layout = html.Div([
 ])
 
 index_page = html.Div([
-    html.H1('Lab 3', style={'text-align': 'center'}),
-    dcc.Link('Navigate to Correlation Matrix', href='/correlation-matrix'),
+    html.H1('NYC Public School Demographics (2006-2018)', style={'text-align': 'center'}),
+    dcc.Link('Basic Visualizations', href='/basic-visualizations'),
     html.Br(),
-    dcc.Link('Navigate to Scatter Plot Matrix', href='/scatter-plot-matrix'),
+    dcc.Link('Advanced Displays', href='/advanced-displays'),
     html.Br(),
-    dcc.Link('Navigate to Parallel Coordinates Display', href='/parallel-coordinates-display'),
-    html.Br(),
-    dcc.Link('Navigate to PCA & Scree Plot', href='/pca-scree-plot'),
-    html.Br(),
-    dcc.Link('Navigate to Biplot', href='/biplot'),
-    html.Br(),
-    dcc.Link('Navigate to MDS Display of Data', href='/MDS-display-data'),
-    html.Br(),
-    dcc.Link('Navigate to MDS Display of Attributes', href='/MDS-display-attributes'),
+    dcc.Link('Interactive Dashboard', href='/interactive-dashboard'),
 ])
 
 correlation_matrix_layout = html.Div([
     html.H1('Correlation Matrix Page', style={'text-align': 'center'}),
     dcc.Graph(
-        figure=px.imshow(dataset2.corr(method='pearson'), color_continuous_scale='Picnic', range_color=[-1, 1],
+        figure=px.imshow(df.corr(method='pearson'), color_continuous_scale='Picnic', range_color=[-1, 1],
                          labels=dict(color="corr"), title="7x7 Correlation Matrix", origin='lower')
     ),
     html.Br(),
@@ -113,9 +111,9 @@ correlation_matrix_layout = html.Div([
 scatter_plot_layout = html.Div([
     html.H1('Scatter Plot Page', style={'text-align': 'center'}),
     dcc.Graph(
-        figure=px.scatter_matrix(dataset2,
-                              dimensions=["total_enrollment", "female_num", "male_num", "asian_num", "hispanic_num"],
-                              title="5x5 Scatter Plot Matrix", labels={col: col.replace('_', ' ') for col in dataset2.columns})
+        figure=px.scatter_matrix(df,
+                                 dimensions=["total_enrollment", "female_num", "male_num", "asian_num", "hispanic_num"],
+                                 title="5x5 Scatter Plot Matrix", labels={col: col.replace('_', ' ') for col in df.columns})
     ),
     html.Br(),
     dcc.Link('Go back to home', href='/'),
@@ -124,16 +122,16 @@ scatter_plot_layout = html.Div([
 parallel_coordinates_layout = html.Div([
     html.H1('Parallel Coordinates Page', style={'text-align': 'center'}),
     dcc.Graph(
-        figure=px.parallel_coordinates(dataset2,
-                                    dimensions=['total_enrollment', 'male_num', 'female_num', 'asian_num', 'white_num', 'hispanic_num', 'black_num'],
-                                    labels={"total_enrollment": "Enrollment", "male_num": "Male", "female_num": "Female",
-                                            "asian_num": "Asian", "white_num": "White", "hispanic_num": "Hispanic", "black_num": "Black"},
-                                    title="7 Axes Parallel Coordinate Display for Numerical Data")),
+        figure=px.parallel_coordinates(df,
+                                       dimensions=['total_enrollment', 'male_num', 'female_num', 'asian_num', 'white_num', 'hispanic_num', 'black_num'],
+                                       labels={"total_enrollment": "Enrollment", "male_num": "Male", "female_num": "Female",
+                                               "asian_num": "Asian", "white_num": "White", "hispanic_num": "Hispanic", "black_num": "Black"},
+                                       title="7 Axes Parallel Coordinate Display for Numerical Data")),
     dcc.Graph(
-        figure=px.parallel_categories(dataset3,
-                                       dimensions=['DBN', 'name', 'year'],
-                                       labels={"DBN": "District Borough Number", "name": "Name", "year": "Year"},
-                                       title="3 Axes Parallel Coordinate Display for Categorical Data")),
+        figure=px.parallel_categories(df1,
+                                      dimensions=['DBN', 'name', 'year'],
+                                      labels={"DBN": "District Borough Number", "name": "Name", "year": "Year"},
+                                      title="3 Axes Parallel Coordinate Display for Categorical Data")),
     html.Br(),
     dcc.Link('Go back to home', href='/'),
 ])
@@ -172,5 +170,29 @@ mds_attributes_layout = html.Div([
     dcc.Link('Go back to home', href='/'),
 ])
 
+basic_visualizations_layout = html.Div([
+    html.H1('MDS Attributes Page', style={'text-align': 'center'}),
+    html.Br(),
+    dcc.Graph(
+        figure=px.scatter(sop, x=0, y=1, title='MDS Plot (1-|correlation| distance)')),
+    dcc.Link('Go back to home', href='/'),
+])
+
+advanced_displays_layout = html.Div([
+    html.H1('MDS Attributes Page', style={'text-align': 'center'}),
+    html.Br(),
+    dcc.Graph(
+        figure=px.scatter(sop, x=0, y=1, title='MDS Plot (1-|correlation| distance)')),
+    dcc.Link('Go back to home', href='/'),
+])
+
+interactive_dashboard_layout = html.Div([
+    html.H1('MDS Attributes Page', style={'text-align': 'center'}),
+    html.Br(),
+    dcc.Graph(
+        figure=px.scatter(sop, x=0, y=1, title='MDS Plot (1-|correlation| distance)')),
+    dcc.Link('Go back to home', href='/'),
+])
+
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8052)
+    app.run_server(debug=True, port=8050)
